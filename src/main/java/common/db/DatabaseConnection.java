b82b0152835c081a7d67dada1928bebc46853990
Le Tan Vi <papylovevip1@gmail.com>
2026-05-07 08:39:12 +0700

    refresh

--- src/main/java/common/db/DatabaseConnection.java
+++ src/main/java/common/db/DatabaseConnection.java
@@ -5,24 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    
+
     public static Connection getConnection() {
         Connection c = null;
         try {
             // Đăng ký Oracle Driver
             DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
-            
+
             // Cấu hình thông số kết nối
             // "localhost" là máy của bạn, "1521" là cổng mặc định, "orcl" là tên database
 //            String url = "jdbc:oracle:thin:@localhost:1521:orcl"; 
-            String url = "jdbc:oracle:thin:@192.168.88.210:1521:orcl";
+            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
             // Thay đổi Username và Password theo đúng tài khoản Oracle 
-            String username = "system"; 
-            String password = "Admin123"; 
-            
+            String username = "system";
+            String password = "Admin123";
+
             // Thực hiện kết nối
             c = DriverManager.getConnection(url, username, password);
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
