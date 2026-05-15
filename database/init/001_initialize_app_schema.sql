8848c00950844a93c632165c75e4a15b45db3a99
KhoiNguyen8326 <hkhoinguyen83@gmail.com>
2026-05-15 18:55:45 +0700

    fix: make Oracle init scripts compatible

--- database/init/001_initialize_app_schema.sql
+++ database/init/001_initialize_app_schema.sql
@@ -4,7 +4,6 @@ CONNECT appuser/apppass@//localhost/FREEPDB1
 
 @/workspace-database/KhoiTaoCacBang.sql
 @/workspace-database/create_kpi_history_table.sql
-@/workspace-database/migration_uom.sql
 @/workspace-database/migration_invoice_payment.sql
 @/workspace-database/seed_data.sql
 @/workspace-database/seed_kpi_criteria.sql
